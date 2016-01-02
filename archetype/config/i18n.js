394aa7bd609a29f71a6c930fed0576f024b6924b
jaumard <ioquoo3H>
2016-01-02 16:18:24 +0100

    Add default i18n config to archetype

---
+++ archetype/config/i18n.js
@@ -0,0 +1,13 @@
+/**
+ * Internationalization Configuration
+ * (app.config.i18n)
+ *
+ * @see http://trailsjs.io/doc/config/i18n
+ */
+
+module.exports = {
+  /**
+   * Default language
+   */
+  lng: 'en'
+}
