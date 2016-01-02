8cfc5d6ad05770d6527d06344e65e19dd26e9f3e
Yann Bertrand <yann@bertrand.com>
2016-01-03 00:37:30 +0100

    Fix #54

---
+++ archetype/config/i18n.js
@@ -0,0 +1,26 @@
+/**
+ * Internationalization / Localization Settings
+ * (app.config.i18n)
+ *
+ * If your app will touch people from all over the world, i18n (or internationalization)
+ * may be an important part of your international strategy.
+ *
+ *
+ * @see http://trailsjs.io/doc/config/i18n
+ *
+ */
+module.exports = {
+
+  /**
+   * The default locale
+   */
+  lng: 'en',
+
+  /**
+   * The path to the locales
+   */
+  resources: {
+    en: require('./locales/en.json')
+  }
+
+}
