e8a5406d89ee51e8bddb8d034ecd11ab067a1d4b
jaumard <ioquoo3H>
2016-04-06 20:41:12 +0200

    Remove web.js archetype Fix yeoman template

--- archetype/config/main.js
+++ archetype/config/main.js
@@ -16,7 +16,7 @@ module.exports = {
     require('trailpack-core'),
     require('trailpack-repl'),
     require('trailpack-router'),
-    require('<%= trailpacks %>')
+    require('<%- trailpacks %>')
   ],
 
   /**
