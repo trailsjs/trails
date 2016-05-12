cfce29539c3b83ac31427e6f2fdb1288972c7f25
Travis <me@traviswebb.com>
2016-05-11 23:40:45 -0400

    [test] remove console.log

--- test/lib/trails.test.js
+++ test/lib/trails.test.js
@@ -248,7 +248,6 @@ describe('lib.Trails', () => {
     })
     it('should return external modules', () => {
       const modules = lib.Trails.getExternalModules()
-      console.log(modules)
       assert(modules.indexOf(require.resolve('smokesignals')) !== -1)
       assert(modules.indexOf(require.resolve('mocha')) !== -1)
     })
